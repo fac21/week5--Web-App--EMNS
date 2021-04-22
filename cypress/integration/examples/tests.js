9fd2930dd525630812b48b24c7b5e4743be944a6
mikailbadoula <michaeldevfay@gmail.com>
2021-04-22 11:56:10 +0100

    add initial test setup

---
+++ cypress/integration/examples/tests.js
@@ -0,0 +1,11 @@
+
+beforeEach(() => {
+    cy.task("resetDb");
+  });
+
+  describe("show homepage", () => {
+        it("displays a heading", () => {
+            cy.visit("/");
+            cy.contains(park finder);
+        });
+  });
